bf03b1e9e712b8b700b793b7646f8cacb225a8a5
Emin Ulusoy <ulusoyeminn@gmail.com>
2023-09-12 01:59:33 +0300

    groups bitti

--- gym-project/src/components/Groups.jsx
+++ gym-project/src/components/Groups.jsx
@@ -21,24 +21,30 @@ function Groups() {
         </div>
       </div>
       <div className='group hover:bg-[#FFDE17] h-1/3 w-full flex justify-center transition duration-500 ease-in '>
-        <div className='bg-[#FFDE17] h-full w-2/4 flex group-hover:translate-x-40 transition duration-500 ease-in '>
-          <div className='w-2/5 h-full'>
-            <img className=' object-[center_top] h-full w-full object-cover' src="assets/women/w16.png" alt="" />
+        <div className='bg-[#FFDE17] h-full w-2/4 flex justify-evenly group-hover:translate-x-40 transition duration-500 ease-in '>
+          <div className='w-2/5 h-full '>
+            <img className=' object-[center_top] h-full w-full object-cover ' src="assets/women/w16.png" alt="" />
           </div>
           <div className='w-2/5 h-full flex flex-col justify-evenly items-center '>
-            <h3 className='text-black text-4xl font-f6 font-bold text-center'>Crossfit Groups</h3>
-            <p className='text-black font-f2 text-center'>In this group, you work entire body rather than a regional workout.In this way, this group provides rapid calorie burning and helps lose weight.</p>
-            <button className='h-10 w-32 bg-black text-white font-bold font-f2 tracking-wider rounded-lg hover:bg-['>Review</button>
+            <h3 className='text-red-500 text-4xl font-f7 font-bold text-center'>Crossfit Groups</h3>
+            <p className='text-gray-500 font-f2 text-align'>In this group, you work entire body rather than a regional workout.In this way, this group provides rapid calorie burning and helps lose weight.</p>
+            <button className='h-10 w-32 bg-red-500 text-white font-bold font-f2 tracking-wider rounded-lg hover:bg-[#30302f] hover:text-white transition duration-150 ease-in'>Review</button>
           </div>
         </div>
       </div>
       <div className='group hover:bg-red-500 h-1/3 w-full flex justify-center transition duration-500 ease-in '>
         <div className='bg-red-500 h-full w-2/4 flex group-hover:translate-x-40 transition duration-500 ease-in'>
-          <div className='w-2/5 h-full'>
-            
+          <div className='w-3/5 h-full flex flex-col justify-evenly items-center'>
+            <h3 className='text-white text-4xl font-f7 font-bold text-center'>Cardio Groups</h3>
+            <ul className='text-black font-bold list-[disc] text-align'>
+              <li className='font-f2'>High Tempo Running</li>
+              <li className='font-f2 my-2'>Fast Burning Calorie</li>
+              <li className='font-f2'>Cycling With Music</li>
+            </ul>
+            <button className='h-10 w-32 bg-white text-black font-bold font-f2 tracking-wider rounded-lg hover:bg-black hover:text-white transition duration-150 ease-in'>Review</button>
           </div>
-          <div className='w-3/5 h-full '>
-            <img className=' object-[center_top] h-full w-full object-cover' src="assets/women/w11.png" alt="" />
+          <div className='w-2/5 h-full '>
+            <img className='h-full w-full object-scale-down' src="assets/women/w13.png" alt="" />
           </div>
         </div>
       </div>
--- gym-project/src/components/Trainers.jsx
+++ gym-project/src/components/Trainers.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 function Trainers() {
   return (
-    <div className="snap-start h-[calc(100vh-100px)] w-screen bg-yellow-500" id='trainers'>Trainers</div>
+    <div className="snap-start h-[calc(100vh-100px)] w-screen bg-yellow-500 flex" id='trainers'>
+      <div className='border-2 w-1/4 h-full'></div>
+      <div className='border-2 w-3/4 h-full flex items-center'>
+        <div className='h-[400px]  w-[400px] border-2 border-red-500'>
+          <img className='w-full h-full object-scale-down' src="assets/others/o3.png" alt="/" />
+        </div>
+        <div className='h-[400px] grow border-2 border-green-500 items-center snap-x snap-mandatory flex overflow-hidden'>
+          <div className='h-[200px] w-[400px] bg-white snap-start flex shrink-0'></div>
+          <div className='h-[200px] w-[400px] bg-red-500 snap-start flex shrink-0'></div>
+          <div className='h-[200px] w-[400px] bg-red-500 snap-start flex shrink-0'></div>
+          <div className='h-[200px] w-[400px] bg-red-500 snap-start flex shrink-0'></div>
+          <div className='h-[200px] w-[400px] bg-red-500 snap-start flex shrink-0'></div>
+          <div className='h-[200px] w-[400px] bg-red-500 snap-start flex shrink-0'></div>
+          <div className='h-[200px] w-[400px] bg-red-500 snap-start flex shrink-0'></div>
+          <div className='h-[200px] w-[400px] bg-red-500 snap-start flex shrink-0'></div>
+          <div className='h-[200px] w-[400px] bg-red-500 snap-start flex shrink-0'></div>
+         
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
