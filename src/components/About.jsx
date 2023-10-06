2ccd8920d72b0684ed8739b3bdcc824283ae0873
Emin Ulusoy <ulusoyeminn@gmail.com>
2023-10-06 03:53:19 +0300

    about responsive completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,60 +3,65 @@ import CountUp from 'react-countup';
 
 function About() {
 
+  // ! Burası incelenecek
+  // ? Burası incelenecek
+  // * Burası incelenecek
+  // TODO: Burası incelenecek
+
   const [counterOn, setCounterOn] = useState(false);
 
   return (
     <div onMouseOver={() => setCounterOn(true)} onMouseOut={() => setCounterOn(false)} className="snap-start  h-[calc(100vh-100px)] w-screen bg-white flex justify-center" id='about'>
-      <div className='h-full w-3/4 relative'>
+      <div className='h-full w-full lg:w-3/4 relative'>
         <img className='absolute h-full w-full object-cover  brightness-[0.6]' src="assets/women/w2.jpg" alt="" />
-        <div className='h-full w-full absolute flex'>
-          <div className='w-1/2 h-full flex flex-col'>
-            <div className='h-[45%] w-full flex flex-col items-center justify-end'>
-              <h1 className='text-g lg:text-6xl font-f2 font-bold '>Who We Are ?</h1>
-              <p className='text-white lg:text-xl font-f4  text-center px-24 mt-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam cumque eveniet aliquid rem reprehenderit architecto alias deserunt beatae at sit culpa voluptas minus quas, ab aliquam, repudiandae ut cupiditate molestias! Culpa inventore eum iste esse adipisci possimus earum neque voluptates.</p>
+        <div className='h-full w-full absolute flex md:flex-row flex-col'>
+          <div className='w-full h-1/2 sm:h-1/2 md:w-1/2 md:h-full flex md:flex-col sm:flex-row '>
+            <div className='md:h-[45%] h-full sm:w-1/2 w-full  md:w-full flex flex-col items-center justify-center md:justify-end'>
+              <h1 className='text-g    text-4xl sm:text-4xl  md:text-4xl  xl:text-5xl  2xl:text-7xl font-f2 font-bold '>Who We Are ?</h1>
+              <p className='text-white text-sm  sm:text-sm md:text-sm lg:text-base  xl:text-base 2xl:text-xl  font-f4  text-center px-3 sm:px-7  md:px-16 lg:px-9 mt-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam cumque eveniet aliquid rem reprehenderit architecto alias deserunt beatae at sit culpa voluptas minus quas, ab aliquam, repudiandae ut cupiditate molestias! Culpa inventore eum iste esse adipisci possimus earum neque voluptates.</p>
             </div>
-            <div className='h-[55%] w-full flex justify-center items-center'>
-              <div className='h-3/4 w-full flex justify-center items-center relative'>
-                <img className='h-3/4 w-1/3 object-cover absolute z-20 left-20 rounded-lg border-2 shadow-2xl shadow-black' src="assets/women/w4.jpg" alt="" />
-                <img className='h-3/4 w-1/3 object-cover absolute z-30  rounded-lg border-2 shadow-2xl shadow-black scale-125' src="assets/women/w3.jpg" alt="" />
-                <img className='h-3/4 w-1/3 object-cover absolute z-10 right-20  rounded-lg border-2 shadow-2xl shadow-black' src="assets/women/w6.jpg" alt="" />
+            <div className='hidden md:h-[55%] h-full w-1/2  md:w-full sm:flex justify-center items-center'>
+              <div className='h-3/4 w-full flex justify-center items-center relative '>
+                <img className='xl:h-3/4 md:h-4/6 sm:h-[60%] h-3/4 w-1/3 object-cover absolute z-20 xl:left-20  lg:left-12 md:left-14 left-5  rounded-lg border-2 shadow-2xl shadow-black' src="assets/women/w4.jpg" alt="" />
+                <img className='xl:h-3/4 md:h-4/6 sm:h-[60%] h-3/4 w-1/3 object-cover absolute z-30  rounded-lg border-2 shadow-2xl shadow-black scale-125' src="assets/women/w3.jpg" alt="" />
+                <img className='xl:h-3/4 md:h-4/6 sm:h-[60%] h-3/4 w-1/3 object-cover absolute z-10 xl:right-20 lg:right-12  md:right-14 right-5   rounded-lg border-2 shadow-2xl shadow-black' src="assets/women/w6.jpg" alt="" />
               </div>
             </div>
             
           </div>
-          <div className='w-1/2 h-full flex flex-col items-center justify-center '>
-            <div className='w-full h-1/2  flex flex-col items-center justify-center'>
+          <div className='w-full h-1/2 sm:h-1/2 md:w-1/2 md:h-full flex  md:flex-col items-center justify-center pb-16 sm:pb-0  '>
+            <div className='hidden  w-full h-1/2  md:flex flex-col items-center justify-center'>
               
             </div>
-            <div className='w-full h-1/2  flex justify-evenly items-center'>
-              <div className='w-1/4 h-full flex flex-col justify-center items-center'>
-                <i className="fa-solid fa-face-smile-beam text-g text-3xl "></i>
-                <h1 className='text-white font-f2 text-4xl m-5'>
+            <div className='w-full h-full sm:h-1/2  flex flex-col sm:flex-row justify-evenly items-center md:pr-4 xl:pr-0 '>
+              <div className='w-full h-1/4 sm:w-1/4 sm:h-full flex flex-row sm:flex-col justify-center items-center '>
+                <i className="fa-solid fa-face-smile-beam text-g text-3xl sm:text-3xl md:text-2xl  xl:text-3xl  2xl:text-4xl"></i>
+                <h1 className='text-white font-f2 text-xl sm:text-2xl md:text-lg  xl:text-3xl 2xl:text-4xl m-5'>
                   {counterOn ? <CountUp start={0} end={800} duration={3} delay={0} /> : 0} +
                 </h1>
-                <p className='text-g font-f2 text-md'>Our Members</p>
+                <p className='text-g font-f2 text-base sm:text-base md:text-xs xl:text-sm 2xl:text-lg'>Our Members</p>
               </div>
-              <div className='w-1/4 h-full flex flex-col justify-center items-center'>
-                <i className="fa-solid fa-people-group text-g text-3xl"></i>
-                <h1 className='text-white font-f2 text-4xl m-5'>
+              <div className='w-full h-1/4 sm:w-1/4 sm:h-full flex flex-row sm:flex-col justify-center items-center'>
+                <i className="fa-solid fa-people-group text-g text-3xl sm:text-3xl md:text-2xl  xl:text-3xl 2xl:text-4xl"></i>
+                <h1 className='text-white font-f2 text-xl sm:text-2xl md:text-lg  xl:text-3xl 2xl:text-4xl m-5'>
                   {counterOn ? <CountUp start={0} end={60} duration={3} delay={0} /> : 0} +
                 </h1>
-                <p className='text-g font-f2 text-md'>Best Coach</p>
+                <p className='text-g font-f2 text-base sm:text-base md:text-xs xl:text-sm 2xl:text-lg'>Best Coach</p>
               </div>
-              <div className='w-1/4 h-full flex flex-col justify-center items-center'>
-                <i className="fa-solid fa-dumbbell text-g text-3xl"></i>
-                <h1 className='text-white font-f2 text-4xl m-5'>
+              <div className='w-full h-1/4 sm:w-1/4 sm:h-full flex flex-row sm:flex-col justify-center items-center'>
+                <i className="fa-solid fa-dumbbell text-g text-3xl sm:text-3xl md:text-2xl xl:text-3xl 2xl:text-4xl"></i>
+                <h1 className='text-white font-f2 text-xl  sm:text-2xl md:text-lg xl:text-3xl 2xl:text-4xl m-5'>
                   {counterOn ? <CountUp start={0} end={20} duration={3} delay={0} /> : 0} +
                 </h1>
-                <p className='text-g font-f2 text-md'>Gym Center</p>
+                <p className='text-g font-f2  text-base sm:text-base md:text-xs xl:text-sm 2xl:text-lg'>Gym Center</p>
                 
               </div>
-              <div className='w-1/4 h-full flex flex-col justify-center items-center'>
-                <i className="fa-solid fa-clipboard-list text-g text-3xl"></i>
-                <h1 className='text-white font-f2 text-4xl m-5'>
+              <div className='w-full h-1/4 sm:w-1/4 sm:h-full flex flex-row sm:flex-col justify-center items-center'>
+                <i className="fa-solid fa-clipboard-list text-g text-3xl sm:text-3xl md:text-2xl  xl:text-3xl 2xl:text-4xl"></i>
+                <h1 className='text-white font-f2 text-xl  sm:text-2xl md:text-lg xl:text-3xl 2xl:text-4xl m-5'>
                   {counterOn ? <CountUp start={0} end={50} duration={3} delay={0} /> : 0} +
                 </h1>
-                <p className='text-g font-f2 text-md'>Fitness Program</p>
+                <p className='text-g font-f2  text-base sm:text-base md:text-xs xl:text-sm 2xl:text-lg'>Fitness Program</p>
               </div>
               
               
